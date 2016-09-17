7a4afba113813269bc66791715243194e2702660
Harshal <harshalvarday1@gmail.com>
2016-09-18 00:41:27 +0530

    Add CORS support

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var cors = require('cors')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
